ec3ffa8cb2590c9a83deeac865b0bb89b22f7e91
pavankat@gmail.com <pavankat@gmail.com>
2013-01-08 02:16:32 -0500

    finished the matchers lesson

--- test_driven_dev_ruby/spec/team_spec.rb
+++ test_driven_dev_ruby/spec/team_spec.rb
@@ -1,8 +1,32 @@
 require_relative "../lib/team.rb"
 
 describe Team do
-	it "has a name"
-	it "has a list of players"
-	it "is favored if it has a celebrity in it"
-	it "complains if there is a bad word in the name"
+	
+	it "has a name" do
+		# make a new Team object and ...
+		Team.new("Random name").should respond_to :name
+	end
+	
+	it "has a list of players" do
+		Team.new("Random name").players.should be_kind_of Array
+	end   
+
+	it "is favored if it has a celebrity in it" do 
+		Team.new("Random name", [ "George Clooney", "Jose Mota" ]).should be_favored
+	end
+	
+	it "complains if there is a bad word in the name" do
+		expect { Team.new("Crappy name") }.to \
+		raise_error
+	end
+
+	context "given a bad list of players" do
+		let(:bad_players) { {} }
+
+		it "fails to create given a bad player list" do
+			expect { Team.new("Random name", bad_players) }.to \
+			raise_error
+		end
+
+	end
 end
